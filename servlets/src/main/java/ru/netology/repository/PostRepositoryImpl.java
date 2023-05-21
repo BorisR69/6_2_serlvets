d8bcc11b18fb6970682a52bc34e364f8f48923f8
Boris Rozov <outsource@list.ru>
2023-05-21 21:02:34 +0300

    Счетчик сделан с применением типа AtomicLong

--- servlets/src/main/java/ru/netology/repository/PostRepositoryImpl.java
+++ servlets/src/main/java/ru/netology/repository/PostRepositoryImpl.java
@@ -6,12 +6,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
 public class PostRepositoryImpl implements PostRepository{
 
   private final List<Post> posts = Collections.synchronizedList(new ArrayList<>());
-  private long count = 1;
+  private AtomicLong count = new AtomicLong(1);
   @Override
   public List<Post> all() {
     return posts;
@@ -26,8 +27,8 @@ public class PostRepositoryImpl implements PostRepository{
   @Override
   public Post save(Post post) {
     if (post.getId() == 0) {
-      post.setId(count);
-      count++;
+      post.setId(count.longValue());
+      count.addAndGet(1);
       posts.add(post);
       return post;
     }
